879434a58a66e966da7db992c525c96187c38e3b
vladimir <medvedev@gorsetitomsk.ru>
2024-06-18 14:58:57 +0700

    diplom

---
+++ target.tf
@@ -0,0 +1,14 @@
+resource "yandex_alb_target_group" "netology" {
+  name           = "netology"
+
+  target {
+    subnet_id    = "e9b1120d0k8i4m1nm7h9"
+    ip_address   = "192.168.10.14"
+  }
+
+  target {
+    subnet_id    = "e2lmv72g7uvgocknu92u"
+    ip_address   = "192.168.20.29"
+  }
+  
+}
